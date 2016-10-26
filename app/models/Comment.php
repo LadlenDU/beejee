66a7cde67567fd31fcafa6b6da32c118acc84316
Dolgov Dmitriy <d.dolgov@i2013.ru>
2016-10-26 18:06:53 +0300

    дополнения в классе Comment

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(APP_DIR . 'components/' . DATABASE_CLASS . '.php');
+require_once(APP_DIR . 'components/DB.php');
 
 /**
  * Class Comment
@@ -15,19 +15,31 @@ class Comment
      */
     public static $tableName = 'comment';
 
-    /*static public function GetAllCities()
+    static public function getComments()
     {
-        $className = DATABASE_CLASS;
-        $db = new $className;
-        $res = $db->selectQuery("SELECT * FROM " . self::$tableName);
-        return $res;
-    }*/
+        $orderBy = (isset($_REQUEST['comments']['order']['by']) && in_array(
+                self::getValidOrderFields(),
+                $_REQUEST['comments']['order']['by'],
+                true
+            )) ? $_REQUEST['comments']['order']['by'] : 'created';
 
-    static public function GetComments()
-    {
-        $className = DATABASE_CLASS;
-        $db = new $className;
-        $res = $db->selectQuery('SELECT * FROM ' . self::$tableName . ' LIMIT');
+        $orderDir = (isset($_REQUEST['comments']['order']['dir']) && DB::obj()->ifValidOrderDirection(
+                $_REQUEST['comments']['order']['dir']
+            )) ? $_REQUEST['comments']['order']['dir'] : 'DESC';
+
+        $res = DB::obj()->selectQuery(
+            'SELECT * FROM ' . self::$tableName . " WHERE `status` = 'APPROVED' ORDER BY $orderBy $orderDir"
+        );
         return $res;
+    }
+
+    /**
+     * Поля по которым сортируем.
+     *
+     * @return array
+     */
+    static private function getValidOrderFields()
+    {
+        return array('created', 'name', 'email');
     }
 }
