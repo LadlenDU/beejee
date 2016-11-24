fe3ce5156248565ce7da33112ce8fc607b11dc76
Dolgov Dmitriy <d.dolgov@i2013.ru>
2016-11-24 17:16:42 +0300

    фикс ConfigHelper

--- app/helpers/ConfigHelper.php
+++ app/helpers/ConfigHelper.php
@@ -4,14 +4,21 @@ class ConfigHelper extends SingletonHelper
 {
     protected $config;
 
-    protected function __construct()
-    {
-        require_once(APP_DIR . '/config/Common.php');
-        $this->config = $GLOBALS['config'];
-    }
+    protected $hasAutoloader;
 
     public function getConfig()
     {
+        if (!$this->config)
+        {
+            $this->config = require_once(APP_DIR . 'config/Common.php');
+        }
+
+        if (!$this->hasAutoloader && function_exists('__autoload'))
+        {
+            $this->hasAutoloader = true;
+            $this->config = array_merge_recursive($this->config, require_once(APP_DIR . 'config/CommonRequireAutoload.php'));
+        }
+
         return $this->config;
     }
 }
